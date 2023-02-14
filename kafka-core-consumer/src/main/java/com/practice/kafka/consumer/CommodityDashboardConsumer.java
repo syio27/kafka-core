f6d23ff3e9acc7edaf97203b907f52dbf3152a45
Baglan Sagidulla <kxelly@mail.ru>
2023-02-14 12:35:14 +0100

    ConsumerGroups - Dashboard and Notifications consumers

---
+++ kafka-core-consumer/src/main/java/com/practice/kafka/consumer/CommodityDashboardConsumer.java
@@ -0,0 +1,25 @@
+package com.practice.kafka.consumer;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.practice.kafka.entity.Commodity;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CommodityDashboardConsumer {
+
+    private static final Logger LOG = LoggerFactory.getLogger(CommodityDashboardConsumer.class);
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @KafkaListener(topics = "t-commodity", groupId = "cg-dashboard")
+    public void consume(String message) throws JsonProcessingException {
+        var commodity = objectMapper.readValue(message, Commodity.class);
+        LOG.info("Dashboard logic for: {}", commodity);
+    }
+}
